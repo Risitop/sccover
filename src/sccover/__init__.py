9ae7d8c8d70eeced426f6287c49faf8169746b74
risitop <aziz.fouche@gmail.com>
2022-10-14 11:30:18 +0200

    first commit

---
+++ src/sccover/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+from .graph import random_sampling, vertex_cover, get_closest_anchor
+from .stats import info
+
+__all__ = ["random_sampling", "vertex_cover", "get_closest_anchor", "info"]
